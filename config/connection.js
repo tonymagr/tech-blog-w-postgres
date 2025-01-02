2a2906c821db13409f53fb546d7a9a95d8cefbe7
Tony Magrady <tonymagrady@gmail.com>
2025-01-01 16:06:19 -0800

    corrections after submitting Heroku support request. connection to deployed prod code was to localhost, which may not be correct. simplified connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,59 +1,26 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
+// const { Client } = require('pg');
 
-console.log('-- Start connection. --');
-
-const sequelize = new Sequelize(
-  process.env.DATABASE,
-  process.env.USER,
-  process.env.PASSWORD,
-  // process.env.DATABASE_URL, 
-  {
-    host: 'localhost',
-    // host: '127.0.0.1',
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    },
-    port: 5432
+const sequelize = new Sequelize({
+  connectionString: process.env.DATABASE_URL,
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
   }
+}
 );
 
-console.log('-- Testing authentication. --');
-
 sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
+.authenticate()
+.then(() => {
+  console.log('Connection has been established successfully.');
+})
+.catch(err => {
+  console.error('Unable to connect to the database:', err);
+});
 
 module.exports = sequelize;
-
-
-// const Sequelize = require('sequelize');
-// require('dotenv').config();
-
-// const sequelize = new Sequelize(
-//   process.env.DATABASE,
-//   process.env.USER,
-//   process.env.PASSWORD,
-
-//   {
-//     host: 'localhost',
-//     // host: '127.0.0.1',
-//     dialect: 'postgres',
-//     port: 5432,
-//     ssl: true
-//   }
-// );
-
-// console.log("connection url:: ",process.env.DATABASE_URL);
-// console.log("connectionString:: ",pool.connectionString);
-
-// module.exports = sequelize;
